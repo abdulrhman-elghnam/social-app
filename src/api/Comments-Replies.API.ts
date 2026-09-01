e6b4e42b8f13b344c1c8db6ac17d7ca07c33dd56
Abdulrhman El-ghnam <abdulrhman.mohamed.atya@gmail.com>
2026-09-01 19:15:16 +0300

    fix: Update API URL handling and clean up comment creation logic

--- src/api/Comments-Replies.API.ts
+++ src/api/Comments-Replies.API.ts
@@ -2,8 +2,7 @@ import axios from "axios";
 import { useAuthStore } from "@/store/authStore";
 
 const getToken = () => useAuthStore.getState().token;
-const url = import.meta.env.VITE_API_URL || 'https://route-posts.routemisr.com';
-
+const url = import.meta.env.VITE_API_URL;
 export interface CommentsParams {
     page?: number;
     limit?: number;
@@ -18,14 +17,11 @@ export function getPostComments(postId: string, params?: CommentsParams) {
     });
 }
 
-// Data can be FormData (if image included) or JSON { text: "..." }
 export function createComment(postId: string, data: any) {
-    // Determine if data is FormData to set the correct content type automatically (axios handles this for FormData)
+
     return axios.post(`${url}/posts/${postId}/comments`, data, {
         headers: {
             Authorization: `Bearer ${getToken()}`,
-            // If it's FormData, let Axios set Content-Type correctly,
-            // otherwise 'application/json' is default
             ...(data instanceof FormData ? { "Content-Type": "multipart/form-data" } : {}),
         },
     });
